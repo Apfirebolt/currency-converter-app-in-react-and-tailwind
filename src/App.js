a7efeeda7be80de47069b0071228aa9721053b1c
Amit P <aspper20@gmail.com>
2022-08-13 22:33:43 +0530

    added screens, react query and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./screens/Home";
+import CurrencyCompare from "./screens/CurrencyCompare";
+import CurrencyList from "./screens/CurrencyList";
+import CurrencyDetail from "./screens/CurrencyDetail";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<HomePage/>} exact />
+          <Route path="/compare" element={<CurrencyCompare/>} />
+          <Route path="/detail" element={<CurrencyDetail/>} />
+          <Route path="/list" element={<CurrencyList/>} />
+        </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
